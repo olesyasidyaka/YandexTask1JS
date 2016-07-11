c0d98e5865dcab8e7683fabfe94fde643cffb665
Olesya Dragalova <olesyadragalova@Kirills-MacBook-Pro.local>
2016-07-11 15:32:56 +0300

    level 1 finished

--- js/doorOthers.js
+++ js/doorOthers.js
@@ -79,70 +79,40 @@ function Door1(number, onUnlock) {
 
     function _onButtonPointerDown(e) {
         e.target.classList.add('door-riddle__button_pressed');
-
-        //checkCondition.apply(this);
     }
 
     function _onButtonPointerUp(e) {
         e.target.classList.remove('door-riddle__button_pressed');
     }
 
+    // var left = 18;
+    // var top = 248;
     var left, top;
-    left = 18;
-    top = 248;
-    var x, y;
-    function _onButtonPointerMove(e) {
-        x = e.clientX - 32;
-        y = e.clientY - 32;
-        //update();
-        window.requestAnimationFrame(update);
-    }
-
     var alpha = Math.PI;
     var xc = 160;
     var yc = 280;
     var radius = 110;
+    function _onButtonPointerMove(e) {
+        left = e.clientX - 32;
+        top = e.clientY - 32;
+        checkCondition.apply(this);
+        window.requestAnimationFrame(update);
+    }
+
     function update() {
-        // var dx = x - left;
-        // var dy = y - top;
-        // var dr = Math.sqrt(dx*dx + dy*dy);
-        // if (dr < 5)
-        //     dr = 0;
-        // var sig = 1;
-        // if ((alpha > 0 && alpha < Math.PI/2 && dx > 0 && dy < 0) ||
-        //     (alpha > Math.PI/2 && alpha < Math.PI && dx > 0 && dy > 0) ||
-        //     (alpha > Math.PI && alpha < 3*Math.PI/2 && dx < 0 && dy > 0) ||
-        //     (alpha > 3*Math.PI && alpha < 2*Math.PI && dx < 0 && dy < 0))
-        //     sig = -1;
-        //
-        // console.log(sig);
-        // var gamma = dr/radius * sig;
-        // alpha += gamma;
         left = xc + radius * Math.cos(alpha) - 32;
         top = yc + radius * Math.sin(alpha) - 32;
         $(buttons[0]).css('left', left + 'px');
         $(buttons[0]).css('top', top + 'px');
-        x = left;
-        y = top;
-        alpha += 5 * Math.PI / 180 % (2 * Math.PI);
-        //window.requestAnimationFrame(update);
+        alpha += 5 * Math.PI / 180;
     }
 
     /**
      * Проверяем, можно ли теперь открыть дверь
      */
     function checkCondition() {
-        var isOpened = true;
-        // buttons.forEach(function(b) {
-        //     if (!b.classList.contains('door-riddle__button_pressed')) {
-        //         isOpened = false;
-        //     }
-        // });
-
-        // Если все три кнопки зажаты одновременно, то откроем эту дверь
-        if (isOpened) {
+        if (alpha > 3*Math.PI)
             this.unlock();
-        }
     }
 
   
